314fd89593bed649b0605402358ed136f03367da
kkj6670 <jjy7201@gmail.com>
2020-09-15 22:43:02 +0900

    Updates

---
+++ precache-manifest.9f4f7ecba5d04aea582763b494e6dbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8011f6f4e2e7f072b51f9e7d39972a",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "0997735cef83786bdf75",
+    "url": "/exchange-app/static/js/2.c71548ba.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/exchange-app/static/js/2.c71548ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8306d3a95c022d62b8",
+    "url": "/exchange-app/static/js/main.fbb19698.chunk.js"
+  },
+  {
+    "revision": "60fe426c58867b15c388",
+    "url": "/exchange-app/static/js/runtime-main.2db9f9a0.js"
+  },
+  {
+    "revision": "5f6ca1004bdcca8cc4d15e728bfe1b65",
+    "url": "/exchange-app/static/media/apex_fulltext_logo.5f6ca100.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f851c364ddc30568427c3e119eb4e25.js"
+  "/exchange-app/precache-manifest.9f4f7ecba5d04aea582763b494e6dbe9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exchange-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
