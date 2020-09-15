faf54fb6a48da7595f987981b9a145a8d5b52b96
kkj6670 <jjy7201@gmail.com>
2020-09-15 23:24:03 +0900

    Updates

--- precache-manifest.405921bd8d849d1c5dc307f427bef975.js
+++ precache-manifest.405921bd8d849d1c5dc307f427bef975.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b528b175744e37b3b44a28d68fac50e3",
+    "revision": "6840f6cf625a3015db99be1062f0c5a7",
     "url": "/exchange-app/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/exchange-app/static/js/2.ed377b2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59f9269061a6502d9a5d",
-    "url": "/exchange-app/static/js/main.fdd5f450.chunk.js"
+    "revision": "3c3b1f38c2a454a57edf",
+    "url": "/exchange-app/static/js/main.669e3020.chunk.js"
   },
   {
     "revision": "60fe426c58867b15c388",
